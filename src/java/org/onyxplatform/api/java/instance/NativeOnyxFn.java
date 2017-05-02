e04be632f341c7f88d0cc0dd282dfbad8d78970d
George Kierstein <gateway.spectacle@gmail.com>
2017-05-02 16:34:05 -0400

    Fleshing out classes

--- src/java/org/onyxplatform/api/java/instance/NativeOnyxFn.java
+++ src/java/org/onyxplatform/api/java/instance/NativeOnyxFn.java
@@ -4,6 +4,17 @@ import clojure.lang.IPersistentMap;
 
 public abstract class NativeOnyxFn extends OnyxFn {
 
+	protected static native IPersistentMap loadNative(IPersistentMap m);
+	protected static native boolean unloadNative(IPersistentMap m);
+
+	public static IPersistentMap loadNativeResources(IPersistentMap m) {
+		return null;
+	}
+
+	public static boolean unloadNativeResources(IPersistentMap args) {
+		return true;
+	}
+
 	public NativeOnyxFn(IPersistentMap m) {
 		super(m);
 	}
